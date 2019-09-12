01df0c8279e89bbca2605525d12b3eeb2208c22c
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-12 19:14:16 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in lol_champion_stats.gemspec
+gemspec
---
+++ lib/lol_champion_stats.rb
@@ -0,0 +1,6 @@
+require "lol_champion_stats/version"
+
+module LolChampionStats
+  class Error < StandardError; end
+  # Your code goes here...
+end
