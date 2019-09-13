2d9ea5e100956f668897548e36762d83e90418af
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-13 21:46:01 +0000

    Finally got something to run! Welcome summoner!

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,8 +2,8 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-require_relative "lol_champion_stats/version"
-
+require_relative "./lol_champion_stats/version"
+require_relative "./lol_champion_stats/cli"
 
 module LolChampionStats
   class Error < StandardError; end
---
+++ lib/lol_champion_stats/cli.rb
@@ -0,0 +1,11 @@
+class CLI
+
+  def run
+    puts "Welcome Summoner!"
+    puts ""
+    puts "Who shall you choose to be your Champion?"
+    puts ""
+    puts "Choose one to view their strengths and their weaknesses."
+  end
+
+end 
