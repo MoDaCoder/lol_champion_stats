025ae2745cd9d25267c55941ee017e93e6742281
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-13 22:05:58 +0000

    Added to scraper.rb

--- lib/lol_champion_stats/scraper.rb
+++ lib/lol_champion_stats/scraper.rb
@@ -1,3 +1,14 @@
+class Scraper
+
+  BASE_URL = "https://na.op.gg"
+
+  def self.scraper_champion_stats
+     index_page = BASE_URL = "/champion/statistics"
+     
+  end
+
+end
+
 require "pry"
 require "nokogiri"
 require "open-uri"
@@ -5,9 +16,13 @@ require "open-uri"
 doc= Nokogiri::HTML(open("https://na.op.gg/champion/statistics"))
 champions_arr = doc.css(".champion-index__champion-list").css(".champion-index__champion-item")
 #should be able to iterate through and grab needed info
+#gets champion from array
 first_champ = champions_arr[0]
+#gets the extension
 url_extension = first_champ.css("a")[0].attributes['href'].value
+#gets the role the champion will play
 position_arr = first_champ.css(".champion-index__champion-item__position").map{|position| position.text}
+#gets the name of the champion
 name = first_champ.css(".champion-index__champion-item__name").text
 
 binding.pry
