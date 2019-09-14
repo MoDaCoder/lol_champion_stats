429daf80fa7a0f332966e41a36d47756443c9a2c
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-14 18:04:11 +0000

    fixed bug

--- lib/lol_champion_stats/scraper.rb
+++ lib/lol_champion_stats/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_champion_stats
      index_page = BASE_URL + "/champion/statistics"
 
-     doc = Nokogiri::HTML(open("index_page"))
+     doc = Nokogiri::HTML(open("https://na.op.gg/champion/statistics"))
 
 
     #  binding.pry
@@ -18,14 +18,14 @@ end
 # require "nokogiri"
 # require "open-uri"
 #
-# doc= Nokogiri::HTML(open("https://na.op.gg/champion/statistics"))
-champions_arr = doc.css(".champion-index__champion-list").css(".champion-index__champion-item")
-#should be able to iterate through and grab needed info
-#gets champion from array
-first_champ = champions_arr[0]
-#gets the extension
-url_extension = first_champ.css("a")[0].attributes['href'].value
-#gets the role the champion will play
-position_arr = first_champ.css(".champion-index__champion-item__position").map{|position| position.text}
-#gets the name of the champion
-name = first_champ.css(".champion-index__champion-item__name").text
+# # doc= Nokogiri::HTML(open("https://na.op.gg/champion/statistics"))
+# champions_arr = doc.css(".champion-index__champion-list").css(".champion-index__champion-item")
+# #should be able to iterate through and grab needed info
+# #gets champion from array
+# first_champ = champions_arr[0]
+# #gets the extension
+# url_extension = first_champ.css("a")[0].attributes['href'].value
+# #gets the role the champion will play
+# position_arr = first_champ.css(".champion-index__champion-item__position").map{|position| position.text}
+# #gets the name of the champion
+# name = first_champ.css(".champion-index__champion-item__name").text
