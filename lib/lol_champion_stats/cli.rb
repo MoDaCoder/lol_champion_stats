1f507a89ca2e5123b836b76a4a338f89b9ba5441
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-16 03:12:26 +0000

    Added on sleep times (need exit input)

--- lib/lol_champion_stats/cli.rb
+++ lib/lol_champion_stats/cli.rb
@@ -1,48 +1,47 @@
-require "pry"
 class CLI
   attr_accessor :name, :role
   def summon
-    puts "Welcome Summoner to The League of Legends!"
+    puts "Welcome Summoner to The League of Legends!".light_blue.bold
     puts ""
     sleep 2
-    puts "Who shall you choose to be your Champion in the battle for the Rift?"
+    puts "Who shall you choose to be your Champion in the battle for the Rift?".yellow.bold
     puts ""
     sleep 3
     Scraper.scrape_champion_stats
     champion_select
-    # farewell
-    # binding.pry
+    farewell
   end
 
   def champion_select
     Champions.all.each_with_index do |champion, index|
       puts "#{index + 1}. #{champion.name}".green
+      sleep 0.009
       end
-      sleep 1
-      puts "Choose your champion summoner by number to learn it's role."
-
+      sleep 0.3
+      puts "Choose your champion summoner by number to learn it's role.".yellow.bold
+      # while input = gets.chomp != "exit" I NEED AN EXIT BUTTON 
       input = gets.chomp
       if input.to_i > Champions.all.size || input.to_i <= 0
-        puts "DO NOT BELIEVE THIS IS A GAME SUMMONER. Please try again.".red
+        puts "DO NOT BELIEVE THIS IS A GAME SUMMONER. Please try again.".red.bold
         sleep 3
-        champion_select
-      elsif
-        input == "exit"
-        farewell
-      else
+      end
       champion = Champions.all[input.to_i - 1]
       puts "!!!You now summoned #{champion.name}!!!".light_blue.bold
       puts "#{champion.role}".yellow
       sleep 3
       champion_select
-      end
     end
 
-
   def farewell
+    puts "?!WHAT YOU THINK YOU CAN JUST LEAVE SUMMONER WITHOUT CHOOSING!?".red.bold
+    sleep 3
     puts ""
-    puts "I see you have made your decision."
+    puts "Hmm... I see you have made your decision.".green
+    sleep 3
     puts ""
-    puts "Farewell until the next battle summoner."
+    puts "Very well.".yellow
+    puts ""
+    puts "Farewell until the next battle then summoner.".blue
+    sleep 1
   end
 end
